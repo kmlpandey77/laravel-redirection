c8ffd47a7f2356fefbb30473bb8a3a9a85db8329
Kamal Pandey <kml.pandey77@gmail.com>
2021-02-07 14:19:06 +0545

    change view for laravel 5.5

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,11 +2,10 @@
 
 @forelse ($redirections as $key=>$redirect)
 <form action="{{ route('redirection.destroy', $redirect->id) }}" method="POST" onsubmit="confirm('Are you sure?');">
-    @method('DELETE')
-    @csrf
+    {!! method_field('DELETE') !!}
+    {!! csrf_field() !!}
     <a href="{{ route('redirection.edit', $redirect) }}">Edit </a> |
     <button type="submit">Delete</button>
-    {{-- <a href="#" onclick="confirmMessage('Are you sure?');return false">Delete </a> &nbsp; --}}
     {{ $key+1 }}. {{ $redirect->from_url }} => {{ $redirect->to_url }}
 </form>
 @empty
@@ -14,13 +13,4 @@
 @endforelse
 
 <a href="{{ route('redirection.create') }}">Create</a>
-
-{{-- <script>
-    function confirmMessage(msg) {
-        if (confirm(msg)) {
-            var form = document.getElementById('deleteform');
-            form.submit()
-        }
-    }
-</script> --}}
 
